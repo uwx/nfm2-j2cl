867f3aaccb2ab5bfc8fb398081679d2c39072300
uwx <maxine@cybergal.com>
2025-07-25 21:04:40 +0200

    stuff

--- src/main/java/j2ts_awt_swing/impl/ComponentScaffold.java
+++
@@ -1,7 +0,0 @@
-package j2ts_awt_swing.impl;
-
-import elemental2.dom.HTMLElement;
-
-public abstract class ComponentScaffold extends AbstractHTMLComponent {
-    public HTMLElement htmlElement;
-}
--- src/main/java/j2ts_awt_swing/impl/ToHTMLAble.java
+++
@@ -1,5 +0,0 @@
-package j2ts_awt_swing.impl;
-
-public interface ToHTMLAble {
-    String toHTML();
-}
